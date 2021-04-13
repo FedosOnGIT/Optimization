f7e86c988ffe3c26320c4338d4f3e433cffd46dc
vladi <vladislav.sg@yandex.ru>
2021-04-13 23:52:12 +0300

    Add exception constructors

--- Lab2/src/structures/NotConvexFunctionException.java
+++ Lab2/src/structures/NotConvexFunctionException.java
@@ -3,8 +3,18 @@ package structures;
 import java.io.IOException;
 
 public class NotConvexFunctionException extends IOException {
-
     NotConvexFunctionException(String message) {
         super(message);
+    }
+
+    public NotConvexFunctionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotConvexFunctionException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotConvexFunctionException() {
     }
 }
