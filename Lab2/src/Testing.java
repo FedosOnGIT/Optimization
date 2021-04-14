9de1d3e6c85f0addfab9e6779a36fd2572fdd71a
Admin <hghg@mail.ru>
2021-04-14 06:29:02 +0300

    testing

--- Lab2/src/Testing.java
+++ Lab2/src/Testing.java
@@ -7,22 +7,22 @@ import java.util.List;
 import java.util.Random;
 
 public class Testing {
-    QuadraticFunction generate(double lines, int number) throws NotConvexFunctionException {
-        assert lines >= 1;
-        double[] matrix = new double[number];
+    static QuadraticFunction generateFunction(int dimension, double condition) throws NotConvexFunctionException {
+        assert condition >= 1;
+        double[] matrix = new double[dimension];
         matrix[0] = 2;
-        matrix[number - 1] = lines * 2;
-        for (int i = 1; i < number - 1; i++) {
-            matrix[i] = Math.random() * (lines * 2 - 2) + 2;
+        matrix[dimension - 1] = condition * 2;
+        for (int i = 1; i < dimension - 1; i++) {
+            matrix[i] = Math.random() * (condition * 2 - 2) + 2;
         }
-        double[] vector = new double[number];
-        for (int i = 0; i < number; i++) {
+        double[] vector = new double[dimension];
+        for (int i = 0; i < dimension; i++) {
             vector[i] = Math.random() * 2000;
         }
         return new QuadraticFunction(new DiagonalMatrix(matrix), new Vector(vector), Math.random() * 2000);
     }
 
-    Vector generateVector(int number) {
+    static Vector generateVector(int number) {
         double[] vector = new double[number];
         for (int i = 0; i < number; i++) {
             vector[i] = Math.random() * 2000;
@@ -30,7 +30,23 @@ public class Testing {
         return new Vector(vector);
     }
 
+    static void generate(QuadraticMethod method) throws NotConvexFunctionException {
+        for (int i = 2; i < 100; i++) {
+            for (double k = 1; k < 1000; k += 0.5) {
+                method.minimum(generateFunction(i, k), generateVector(i), 0.001);
+            }
+        }
+    }
+
     public static void main(String[] args) throws NotConvexFunctionException {
-        QuadraticMethod method = new ConjugateGradients();
+        // GradientDescent
+        // System.out.println("Gradient descent");
+        // generate(new GradientDescent());
+        // SteepestDescent
+        // System.out.println("Steepest descent");
+        // generate(new SteepestDescent(new GoldenRatioMethod()));
+        // ConjugateGradients
+        System.out.println("Conjugated gradients");
+        generate(new ConjugateGradients());
     }
 }
