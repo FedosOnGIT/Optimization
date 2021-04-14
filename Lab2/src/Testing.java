7662c55d2edbae808d04b475f8890d44bc77dad2
Admin <hghg@mail.ru>
2021-04-14 04:01:20 +0300

    all three

--- Lab2/src/Testing.java
+++ Lab2/src/Testing.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 public class Testing {
     public static void main(String[] args) throws NotConvexFunctionException {
-        QuadraticMethod method = new GradientDescent();
-        DiagonalMatrix a = new DiagonalMatrix(new double[]{1, 2, 3, 4});
-        System.out.println(a.getMaxEigenvalue());
+        QuadraticMethod method = new ConjugateGradients();
+        QuadraticFunction function = new QuadraticFunction(new DiagonalMatrix(new double[]{2, 2}), new Vector(new double[]{1, 0}), 0);
+        method.minimum(function, new Vector(new double[]{1, 3}), 0.00001).print();
     }
 }
--- Lab2/src/quadraticMethods/ConjugateGradients.java
+++ Lab2/src/quadraticMethods/ConjugateGradients.java
@@ -1,12 +1,26 @@
 package quadraticMethods;
 
+import structures.Matrix;
 import structures.QuadraticFunction;
 import structures.Vector;
 
 public class ConjugateGradients implements QuadraticMethod {
 
     @Override
-    public Vector minimum(QuadraticFunction function, Vector point, double epsilon) {
-        return null;
+    public Vector minimum(final QuadraticFunction function, Vector point, final double epsilon) {
+        Vector gradient = function.applyGradient(point);
+        double gradientLength = gradient.length();
+        Vector slope = gradient.multiply(-1);
+        Matrix A = function.getMatrix();
+        while (gradientLength > epsilon) {
+            double alpha = gradientLength * gradientLength / (A.multiply(slope).multiply(slope));
+            point.add(slope.multiply(alpha));
+            gradient = function.applyGradient(point);
+            double newGradientLength = gradient.length();
+            double beta = newGradientLength * newGradientLength / (gradientLength * gradientLength);
+            slope.multiply(beta).plus(gradient.multiply(-1));
+            gradientLength = newGradientLength;
+        }
+        return point;
     }
 }
