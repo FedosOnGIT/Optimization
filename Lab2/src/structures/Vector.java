264fd0605f349fd0ef1d599715bedbe7692cf014
VladSG <vladislav.sg@yandex.ru>
2021-04-16 00:01:43 +0300

    Fix fedos Vector

--- Lab2/src/structures/Vector.java
+++ Lab2/src/structures/Vector.java
@@ -1,6 +1,7 @@
 package structures;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Vector {
@@ -8,100 +9,105 @@ public class Vector {
      * An array of {@link Double} vector's coo
      */
     private final double[] coordinates;
-    private final int size;
 
     public Vector(final double[] coordinates) {
-        this.size = coordinates.length;
-        this.coordinates = new double[size];
-        IntStream.range(0, size).forEach(i -> this.coordinates[i] = coordinates[i]);
+        this.coordinates = coordinates;
+    }
+
+    public Vector(final Vector other) {
+        coordinates = Arrays.copyOf(other.coordinates, other.size());
     }
 
     public Vector(final int size) {
         if (size <= 0) {
             throw new IllegalArgumentException("Size is less than one!");
         }
-        this.size = size;
         coordinates = new double[size];
         Arrays.fill(coordinates, 0);
     }
 
     public int size() {
-        return size;
+        return coordinates.length;
     }
 
     public double getCoordinate(final int index) {
-        if (index >= size) {
+        if (index >= size()) {
             throw new IllegalArgumentException("Out of vector size");
         }
         return coordinates[index];
     }
 
     public void setCoordinate(final int index, final double value) {
-        if (index >= size) {
+        if (index >= size()) {
             throw new IllegalArgumentException("Out of vector size");
         }
         coordinates[index] = value;
     }
 
     public void add(Vector other) {
-        if (other.size != size) {
-            throw new IllegalArgumentException("Vectors have different size");
+        sizeAssert(other);
+        for (int i = 0; i < size(); ++i) {
+            coordinates[i] += other.getCoordinate(i);
         }
-        IntStream.range(0, size).forEach(i -> coordinates[i] += other.getCoordinate(i));
     }
 
     public void extension(double coefficient) {
-        IntStream.range(0, size).forEach(i -> coordinates[i] *= coefficient);
+        for (int i = 0; i < size(); ++i) {
+            coordinates[i] *= coefficient;
+        }
     }
 
     public Vector plus(Vector other) {
-        assert other.size() == size;
-        Vector result = new Vector(size);
-        result.add(this);
+        sizeAssert(other);
+        Vector result = new Vector(this);
         result.add(other);
         return result;
     }
 
     public Vector multiply(double coefficient) {
-        Vector result = new Vector(size);
+        Vector result = new Vector(size());
         result.add(this);
         result.extension(coefficient);
         return result;
     }
 
     public Double multiply(Vector other) {
-        assert other.size() == size;
+        sizeAssert(other);
         double answer = 0;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size(); i++) {
             answer += other.getCoordinate(i) * getCoordinate(i);
         }
         return answer;
     }
 
     public double length() {
-        double answer = 0;
-        for (int i = 0; i < size; i++) {
-            answer += coordinates[i] * coordinates[i];
-        }
-        return Math.sqrt(answer);
+        return Math.sqrt(
+                Arrays
+                    .stream(coordinates)
+                    .map((x) -> x*x)
+                    .sum()
+        );
     }
 
     public void normalise() {
         double length = length();
-        if (length == 0) {
-            throw new IllegalArgumentException("Can't normalize, because vector length is 0!");
-        }
-        for (int i = 0; i < size; i++) {
-            coordinates[i] /= length;
+        if (length != 0) {
+            for (int i = 0; i < size(); i++) {
+                coordinates[i] /= length;
+            }
         }
     }
 
-    public String print() {
-        StringBuilder vector = new StringBuilder();
-        for (int i = 0; i < size; i++) {
-            vector.append(coordinates[i]);
-            vector.append(" ");
-        }
+    @Override
+    public String toString() {
+        final StringBuilder vector = new StringBuilder();
+        Arrays.stream(coordinates).forEach((x) -> vector.append(x).append(" "));
         return vector.toString();
+    }
+
+    private void sizeAssert(Vector other) {
+        if (other.size() != size()) {
+            throw new IllegalArgumentException("Vectors have different size");
+        }
     }
 }
