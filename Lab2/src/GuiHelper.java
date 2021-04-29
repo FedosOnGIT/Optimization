81e3ceb062efdda00b8008b6dcf3758dd4b3f3c2
VladSG <vladislav.sg@yandex.ru>
2021-04-29 15:17:27 +0300

    GuiHelper linear method choose support

--- Lab2/src/GuiHelper.java
+++ Lab2/src/GuiHelper.java
@@ -1,4 +1,5 @@
 import quadraticMethods.QuadraticMethod;
+import quadraticMethods.SteepestDescent;
 import structures.QuadraticFunction;
 import structures.Vector;
 
@@ -11,31 +12,47 @@ import java.nio.file.Path;
  * @author Vladislav Gusev (vladislav.sg@yandex.ru)
  */
 public class GuiHelper {
+    private static void sizeAssert(int pos, int size, String message) throws IllegalArgumentException {
+        if (pos < 0 || pos >= size)
+            throw new IllegalArgumentException(message + " not in [0;" + size + "]");
+    }
+
     /**
-     * Первый аргумент - номер функции от 1 до 3
+     * Первый аргумент - позиция выбранной функции в списке Testing.FUNCTIONS
      * Второй аргумент - позиция выбранного метода в списке Testing.QUADRATIC_METHODS
      * Третий аргумент - координата x1 стартовой точки
      * Четвёртый аргумент - координата x2 стартовой точки
+     * Пятый аргумент - выбранный метод одномерной оптимизации (опционально)
      *
      * Логи итераций будут записаны в файл .\gui_logs.txt
      */
     public static void main(String[] args) throws IOException {
-        if (args == null || args.length != 4)
-            throw new IllegalArgumentException();
+        if (args == null || args.length != 4 && args.length != 5)
+            throw new IllegalArgumentException("Illegal arguments count");
 
         int funcId = Integer.parseInt(args[0]) - 1;
         int methodId = Integer.parseInt(args[1]);
         double[] cord = new double[2];
         cord[0] = Double.parseDouble(args[2]);
         cord[1] = Double.parseDouble(args[3]);
-        if (funcId < 0 || funcId >= Testing.FUNCTIONS.size())
-            throw new IllegalArgumentException("Function number not in [1;" + Testing.FUNCTIONS.size() + "]");
-        if (methodId < 0 || methodId >= Testing.QUADRATIC_METHODS.size()) {
-            throw new IllegalArgumentException("Method number not in [0;" + (Testing.QUADRATIC_METHODS.size()-1) + "]");
-        }
+        sizeAssert(funcId, Testing.FUNCTIONS.size(), "Function number");
+        sizeAssert(methodId, Testing.QUADRATIC_METHODS.size(), "Method number");
 
         QuadraticFunction function = Testing.FUNCTIONS.get(funcId);
-        QuadraticMethod method = Testing.QUADRATIC_METHODS.get(methodId);
+        QuadraticMethod method;
+        if (methodId == 1) {
+            if (args.length != 5) {
+                throw new IllegalArgumentException("Linear Method number is absent");
+            }
+            int linearMethodId = Integer.parseInt(args[4]);
+            sizeAssert(linearMethodId, Testing.LINEAR_METHODS.size(), "Linear Method number");
+            method = new SteepestDescent(Testing.LINEAR_METHODS.get(linearMethodId));
+        } else {
+            if (args.length != 4) {
+                throw new IllegalArgumentException("Used Quadratic Method not use Linear Methods");
+            }
+            method = Testing.QUADRATIC_METHODS.get(methodId);
+        }
 
         try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(Path.of("gui_logs.txt")))) {
             method.minimum(function, new Vector(cord), 1e-2).write(writer);
