41e512a0eecaa3a4c97cd828aa6517a8d6f595e1
Admin <hghg@mail.ru>
2021-05-19 21:49:45 +0300

    sparse

--- Lab3/src/structures/matrices/SparseMatrix.java
+++ Lab3/src/structures/matrices/SparseMatrix.java
@@ -37,18 +37,27 @@ public class SparseMatrix extends Matrix {
         return null;
     }
 
-    @Override
-    protected double getImpl(final int i, final int j) {
-        if (i == j) {
-            return Diagonal[i];
-        }
+    private int getPosition(final int i, final int j) {
         int x = Math.max(i, j);
         int y = Math.min(i, j);
         int left = Indices[x];
         int right = Indices[x + 1];
         int index = Collections.binarySearch(Positions.subList(left, right), y);
         if (index > -1) {
-            return Triangle.get(left + index);
+            return left + index;
+        } else {
+            return -1;
+        }
+    }
+
+    @Override
+    protected double getImpl(final int i, final int j) {
+        if (i == j) {
+            return Diagonal[i];
+        }
+        int position = getPosition(i, j);
+        if (position > -1) {
+            return Triangle.get(position);
         } else {
             return 0;
         }
@@ -56,7 +65,16 @@ public class SparseMatrix extends Matrix {
 
     @Override
     protected void setImpl(int i, int j, double value) {
-
+        if (i == j) {
+            Diagonal[i] = value;
+            return;
+        }
+        int position = getPosition(i, j);
+        if (position > -1) {
+            Triangle.set(position, value);
+        } else {
+            throw new IllegalArgumentException("Can't set on zero!");
+        }
     }
 
     @Override
@@ -66,11 +84,11 @@ public class SparseMatrix extends Matrix {
 
     @Override
     public int columnsCount() {
-        return 0;
+        return size;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 }
