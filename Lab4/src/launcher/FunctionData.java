12687a64347fc060a7c1793a795cb5646ed0e12d
aleksandr <a.slastin@bk.ru>
2021-06-10 14:49:53 +0300

    add launcher and functionData

---
+++ Lab4/src/launcher/FunctionData.java
@@ -0,0 +1,44 @@
+package launcher;
+
+import structures.Getian;
+import structures.Gradient;
+import structures.matrices.Vector;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class FunctionData {
+//    private final Function<Vector, Double> function;
+//    private final Gradient gradient;
+//    private final Getian getian;
+//
+//    public FunctionData(Function<Vector, Double> function, Gradient gradient, Getian getian) {
+//        this.function = function;
+//        this.gradient = gradient;
+//        this.getian = getian;
+//    }
+//
+//    public FunctionData(BiFunction<Double, Double, Double> function, List<BiFunction<Double, Double, Double>> gradient,
+//                        List<List<BiFunction<Double, Double, Double>>> getian) {
+//        this.function = v -> function.apply(v.get(0), v.get(1));
+//
+//        final BiFunction<Vector, Integer, Double> gradientHelper = (v, i) -> gradient.get(i).apply(v.get(0), v.get(1));
+//        this.gradient = new Gradient(v -> gradientHelper.apply(v, 0), v -> gradientHelper.apply(v, 1));
+//
+//        this.getian =
+//    }
+//
+//    public Function<Vector, Double> getFunction() {
+//        return function;
+//    }
+//
+//    public Gradient getGradient() {
+//        return gradient;
+//    }
+//
+//    public Getian getGetian() {
+//        return getian;
+//    }
+}
---
+++ Lab4/src/launcher/Launcher.java
@@ -0,0 +1,18 @@
+package launcher;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class Launcher {
+
+//
+//    private final List<FunctionData> TASK1_2 = List.of(
+//            new FunctionData(
+//                    v -> {
+//                        double x = v.get(0), y = v.get(1);
+//                        return x * x + y * y - 1.2 * x * y;
+//                    },
+//
+//            )
+//    );
+}
